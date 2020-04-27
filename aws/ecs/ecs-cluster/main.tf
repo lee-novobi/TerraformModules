e5b9f7229c01957dcb1467469faae58e8163ff3f
Duy Ly <duyh@compandsave.com>
2020-04-27 14:01:14 -0700

    assume role

--- aws/ecs/ecs-cluster/main.tf
+++ aws/ecs/ecs-cluster/main.tf
@@ -10,12 +10,12 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-  /*
+
   dynamic "assume_role" {
     for_each = var.aws_assume_role == "" ? [] : [var.aws_assume_role]
-content {
-role_arn = assume_role.value
-}
-}
-  */
+    content {
+      role_arn = assume_role.value
+    }
+  }
+
 }
--- aws/ecs/ecs-service/main.tf
+++ aws/ecs/ecs-service/main.tf
@@ -4,12 +4,12 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-//  dynamic "assume_role" {
-//    for_each = var.aws_assume_role == "" ? [] : [var.aws_assume_role]
-//    content {
-//      role_arn = assume_role.value
-//    }
-//  }
+  dynamic "assume_role" {
+    for_each = var.aws_assume_role == "" ? [] : [var.aws_assume_role]
+    content {
+      role_arn = assume_role.value
+    }
+  }
 }
 
 data "terraform_remote_state" "ecs_cluster" {
